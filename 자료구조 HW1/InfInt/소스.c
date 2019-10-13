7f4914c0390f291d678a8e530dc5255dbc3dcef3
zeebraa00 <zeebraa00@gmail.com>
2019-10-13 15:10:02 +0900

    [edit] HW1 Complete

---
+++ 자료구조 HW1/InfInt/소스.c
@@ -0,0 +1,132 @@
+#pragma warning (disable: 4996)
+#include "stdio.h"
+#define Digit 1001
+
+char temp[Digit]; // input 적절하게 초기화할 때 사용할 것
+char output[Digit] = {'0', }; // 최종 연산 결과
+char tmp[Digit] = {'0', }; // 연산 결과 처리할 때 사용
+char tmp_2[Digit] = { '0', }; // 연산 결과 처리할 때 사용
+int length; // output의 자릿수
+
+void Add(char input1[], char input2[]) {
+	int sum;
+	int carry=0; // 자릿수 올림 변수
+
+	//output 초기화
+	for (int i = 0; i < Digit - 1; i++) {
+		output[i] = '0';
+	}
+	output[Digit - 1] = '\0';
+
+	// 큰 수 더하기
+	for (int i = Digit - 2; i > 0; i--) {
+		sum = (input1[i] - '0') + (input2[i] - '0') + carry;
+		carry = sum / 10;
+		sum = sum % 10;
+		output[i] = (sum + '0');
+	}
+}
+
+void Product(char input1[], char input2[]) {
+	int sum;
+	int carry = 0;
+
+	// 배열 전처리
+	for (int i = 0; i < Digit-1; i++) {
+		output[i] = '0';
+	}
+	output[Digit - 1] = '\0';
+	for (int i = 0; i < Digit-1; i++) {
+		tmp[i] = '0';
+	}
+	tmp[Digit - 1] = '\0';
+	for (int i = 0; i < Digit-1; i++) {
+		tmp_2[i] = '0';
+	}
+	tmp_2[Digit - 1] = '\0';
+
+	// 큰 수 곱하기
+	for (int i = Digit-2; i > 0; i--) {
+		for (int j = Digit-2; j > 0; j--) {
+			sum = (input1[i] - '0') * (input2[j] - '0') + carry;
+			carry = sum / 10;
+			sum = sum % 10;
+			tmp[j-(Digit-2-i)] = sum+'0';
+		}
+		Add(tmp_2, tmp);
+		for (int i = 0; i < Digit; i++) {
+			tmp_2[i] = output[i];
+		}
+		// tmp 초기화
+		for (int i = 0; i < Digit; i++) {
+			tmp[i] = '0';
+		}
+		tmp[Digit - 1] = '\0';
+	}
+	for (int i = 0; i < Digit; i++) {
+		output[i] = tmp_2[i];
+	}
+}
+
+int main() {
+	char input1[Digit];
+	char input2[Digit];
+	char operator;
+
+	// 연산자 입력받기
+	scanf("%c", &operator);
+
+	// input에 정수 입력받기
+	scanf("%s", input1);
+	scanf("%s", input2);
+
+	// 입력받은 정수의 자릿수 계산
+	int len;
+	for (len = 0; input1[len] != '\0'; len++); // len이 자릿수 의미
+
+	// input1 배열 적절히 초기화
+	for ( int i=0 ; i < Digit-len-1 ; i++) {
+		temp[i] = '0'; // 앞을 다 0으로 채우기
+	}
+	for (int i = Digit-len-1 ; i < Digit ; i++) {
+		temp[i] = input1[i-(Digit-len-1)]; // 유효한 숫자들 맨 뒤로 뺴기
+	}
+	for (int i = 0; i < Digit; i++) {
+		input1[i] = temp[i]; // temp에 있던거 input1으로 복붙
+	}
+
+	// input2 배열 적절히 초기화
+	for (len = 0; input2[len] != '\0'; len++); // len이 자릿수 의미
+	for (int i = 0; i < Digit - len - 1; i++) {
+		temp[i] = '0'; // 앞을 다 0으로 채우기
+	}
+	for (int i = Digit - len - 1; i < Digit; i++) {
+		temp[i] = input2[i - (Digit - len - 1)]; // 유효한 숫자들 맨 뒤로 뺴기
+	}
+	for (int i = 0; i < Digit; i++) {
+		input2[i] = temp[i]; // temp에 있던거 input2으로 복붙
+	}
+
+	// 연산 처리
+	if (operator == '+') {
+		Add(input1, input2);
+		for (length = 0; output[length] == '0'; length++);
+		if (length == Digit - 1) {
+			printf("0"); // 결과값==0 일 때 예외처리
+		}
+		else {
+			printf("%s", output + length);
+		}
+	}
+	else if (operator == '*') {
+		Product(input1, input2);
+		for (length = 0; output[length] == '0'; length++);
+		if (length == Digit - 1) {
+			printf("0"); // 결과값==0 일 때 예외처리
+		}
+		else {
+			printf("%s", output + length);
+		}
+	}
+	return 0;
+}
---
+++ 자료구조 HW1/SortWord/소스.c
@@ -0,0 +1,114 @@
+﻿#pragma warning (disable: 4996)
+#include <stdio.h>
+#include <string.h>
+
+int cnt[1000][2];
+int a; // 
+char arr[1000][51];
+char output[1000][51];
+char tmp[51]="";
+char ck[51];
+int len;
+
+int main() {
+	int n;
+
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf("%s", &arr[i]);
+	for (int i = 0; i < n; i++)
+		cnt[i][0] = i; // count배열 넘버링
+	for (int i = 0; i < n; i++)
+	{
+		if (ck[i] == 1) continue;
+		for (int j = 0; j < n; j++)
+		{
+			if (!strcmp(arr[i], arr[j]))
+			{
+				ck[j] = 1; // j번째 단어가 i번째 단어와 같다.
+				cnt[i][1]++; // i번째 단어가 count번 반복된다.
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		int max = cnt[i][1];
+		int max_pos = i, temp;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (cnt[j][1] > max)
+			{
+				max = cnt[j][1]; // count의 최댓값
+				max_pos = j; // count가 최대인 칸의 index
+			}
+		}
+		//여기서부터 count 배열을 내림차순으로 정렬
+		temp = cnt[i][1];
+		cnt[i][1] = cnt[max_pos][1];
+		cnt[max_pos][1] = temp;
+
+	// 이 시점이 되면 빈도수 내림차순으로 인덱스-횟수 를 원소로 갖는 count 정렬 완료
+
+		temp = cnt[i][0];
+		cnt[i][0] = cnt[max_pos][0];
+		cnt[max_pos][0] = temp;
+	}
+
+	//for (int i = 0; i < n; i++)
+	//{
+	//	printf("%d %d\n", cnt[i][0], cnt[i][1]);
+	//}
+
+	// 빈도수가 같을 때 알파벳 순으로 정렬하기
+	for (int i=0; i < n; i ++) {
+		int first=i;
+		int last=i;
+
+		//for (int j = first ; j < n; j++) {
+		//	if (cnt[first][1] == cnt[j][1]) {
+		//		last=j;
+		//	}
+		//}
+
+		while (cnt[first][1] == cnt[last][1] && last < n) {
+			//printf("			%d %d\n", cnt[first][1], cnt[last][1]);
+			last++;
+		}
+		last--;
+
+		//printf("first : %d , last : %d\n", i, last);
+		
+		//strcpy(tmp, arr[cnt[i][0]]);
+
+		//for (int k = 0; i < last - k + 1; i ++) {
+		//	for (int j = first + k + 1; j <= last; j++) {
+		//		if (strcmp(tmp, arr[cnt[j][0]])>0) { // count수가 같고, 뒤에 있는게 더 내림차순이라면
+		//			strcpy(tmp, arr[cnt[j][0]]);
+		//		}
+		//	}
+		//	strcpy(arr[cnt[k][0]], tmp);
+		//	cnt[k][0] = cnt[a][0];
+		//}
+		i += last - first;
+		for (int cycle = first; cycle <= last; cycle++) {
+			for (int temp = first; temp < last; temp++)
+			{
+				if (strcmp(arr[cnt[temp][0]], arr[cnt[temp + 1][0]]) > 0) {
+					strcpy(tmp, arr[cnt[temp][0]]);
+					strcpy(arr[cnt[temp][0]], arr[cnt[temp + 1][0]]);
+					strcpy(arr[cnt[temp + 1][0]], tmp);
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		//printf("인덱스 : %d - 값 : %d \n", cnt[i][0], cnt[i][1]);
+		if (cnt[i][1] != 0) printf("%s\n", arr[cnt[i][0]]);
+	}
+
+	return 0;
+}
